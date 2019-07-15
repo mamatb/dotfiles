3eaa1c5e1e02b9343bdabf7be1637daabcdec073
mamatb <amatbaeza@gmail.com>
2019-07-15 21:32:50 +0000

    long options use for the sake of readability

--- getMostCommonLines.sh
+++ getMostCommonLines.sh
@@ -6,17 +6,17 @@ then
 	then
 		if [[ "${2}" =~ ^[[:digit:]]+$ ]];
 		then
-			sort "${1}" | uniq -c | sort -nr | head -n "${2}";
-			exit 0;
+			sort "${1}" | uniq --count | sort --numeric-sort --reverse | head --lines "${2}"
+			exit 0
 		else
-			echo -e "INPUT ERROR:\n\tsecond argument is not a valid integer";
-			exit 1;
+			echo -e "INPUT ERROR:\n\tsecond argument is not a valid integer"
+			exit 1
 		fi
 	else
-		echo -e "INPUT ERROR:\n\tfirst argument is either not a regular file or not readable";
-		exit 1;
+		echo -e "INPUT ERROR:\n\tfirst argument is either not a regular file or not readable"
+		exit 1
 	fi
 else
-	echo -e "SYNTAX ERROR. USAGE:\n\tgetMostCommonStrings.sh FILENAME NUMBER";
-	exit 1;
+	echo -e "SYNTAX ERROR. USAGE:\n\tgetMostCommonStrings.sh <filename> <number>"
+	exit 1
 fi

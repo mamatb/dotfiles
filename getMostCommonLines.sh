b5c4422e8d7a84acb3e0d1d241822e7e429acc0f
mamatb <amatbaeza@gmail.com>
2019-11-14 14:50:54 +0000

    unnecessary trailing ";" removed, quotes replaced

--- getMostCommonLines.sh
+++ getMostCommonLines.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-if [ "${#}" = "2" ];
+if [ "${#}" = '2' ]
 then
-	if [ -f "${1}" ] && [ -r "${1}" ];
+	if [ -f "${1}" ] && [ -r "${1}" ]
 	then
-		if [[ "${2}" =~ ^[[:digit:]]+$ ]];
+		if [[ "${2}" =~ ^[[:digit:]]+$ ]]
 		then
 			sort "${1}" | uniq --count | sort --numeric-sort --reverse | head --lines "${2}"
 			exit 0
 		else
-			echo -e "INPUT ERROR:\n\tsecond argument is not a valid integer"
+			echo -e 'INPUT ERROR:\n\tsecond argument is not a valid integer'
 			exit 1
 		fi
 	else
-		echo -e "INPUT ERROR:\n\tfirst argument is either not a regular file or not readable"
+		echo -e 'INPUT ERROR:\n\tfirst argument is either not a regular file or not readable'
 		exit 1
 	fi
 else
-	echo -e "SYNTAX ERROR. USAGE:\n\tgetMostCommonStrings.sh <filename> <number>"
+	echo -e 'SYNTAX ERROR. USAGE:\n\tgetMostCommonStrings.sh <filename> <number>'
 	exit 1
 fi
